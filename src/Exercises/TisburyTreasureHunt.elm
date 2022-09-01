34da1c836828ec589cc407b207932ccaed9da101
shbhmj <shbhmkmrjha@gmail.com>
2022-09-01 16:47:59 +0530

    Treasure Hunt Exercise

--- src/Exercises/TisburyTreasureHunt.elm
+++ src/Exercises/TisburyTreasureHunt.elm
@@ -1,33 +1,44 @@
-module Exercises.TisburyTreasureHunt exposing (..)
+module TisburyTreasureHunt exposing (..)
 -- https://exercism.org/tracks/elm/exercises/tisbury-treasure-hunt
 
 type alias TreasureLocation = (Int, Char) 
 
-type alias Treasure = { name: String, location: TreasureLocation}
+type alias Treasure = (String, TreasureLocation)
 
 type alias PlaceLocation = (Char, Int)
 
-type alias Place = { name: String, location: PlaceLocation}
+type alias Place = (String, PlaceLocation)
 
 
 placeLocationToTreasureLocation : PlaceLocation -> TreasureLocation
-placeLocationToTreasureLocation placeLocation =
-    (Tuple.second placeLocation, Tuple.first placeLocation)
+placeLocationToTreasureLocation (col, row) =
+    (row, col)
 
 
 treasureLocationMatchesPlaceLocation : PlaceLocation -> TreasureLocation -> Bool
 treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
-    if (Tuple.first placeLocation == Tuple.second treasureLocation) && (Tuple.second placeLocation == Tuple.first treasureLocation) then
-        True
-    else
-        False
+       placeLocation
+        |> placeLocationToTreasureLocation
+        |> (==) treasureLocation
 
 
 countPlaceTreasures : Place  -> List Treasure -> Int
-countPlaceTreasures place treasures =
-    List.filter (\t-> t.location == (placeLocationToTreasureLocation place.location)) treasures |> List.length
-
+countPlaceTreasures (_, placeLocation) treasures =
+     treasures
+            |> List.map (\(_,treasureLocation)-> treasureLocation)
+            |> List.filter (treasureLocationMatchesPlaceLocation placeLocation)
+            |> List.length
 
 specialCaseSwapPossible : Treasure -> Place  -> Treasure -> Bool
-specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
-    Debug.todo "implement this function"
+specialCaseSwapPossible( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
+     case (foundTreasure, place, desiredTreasure) of
+        ("Brass Spyglass", "Abandoned Lighthouse", _) -> True
+        ("Amethyst Octopus", "Stormy Breakwater", "Crystal Crab") -> True
+        ("Amethyst Octopus", "Stormy Breakwater", "Glass Starfish") -> True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle") ->
+            True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float") ->
+            True
+        
+        _ -> False
+    
